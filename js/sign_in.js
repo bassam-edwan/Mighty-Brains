90743a0caf5521867cf958d648039b38a683f8b8
bassam-edwan <88935668+bassam-edwan@users.noreply.github.com>
2021-09-24 17:42:01 +0300

    Add files via upload

---
+++ js/sign_in.js
@@ -0,0 +1,25 @@
+const url = "http://localhost:500/api/account"
+async function postaccount(name,course,email,number){
+const data = await fetch(url,{
+    method: "POST",
+    headers: {
+        'accept':'application/json',
+        'content-type':'application/json'
+    },
+    body:JSON.stringify({name,course,email,number})
+    
+})
+const updated_data = await  data.json()
+return updated_data
+}
+
+//getting values from form
+
+document.getElementById('sign_in').addEventListener('submit', function(e){
+    e.preventDefault()
+const name = document.getElementById('name').value
+const course = document.getElementById('course').value
+const email = document.getElementById('email').value
+const number = document.getElementById('number').value
+postaccount(name,course,email,number)
+})
